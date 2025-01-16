29eaecdcca95763e91dcb73eae9bfe665ee81c85
khushi <khushiagarwal7821@gmail.com>
2025-01-17 00:44:00 +0530

    add redux persist

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,13 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
 import  useReducer  from './user/userSlice';
+import {persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage";
+import persistStore from 'redux-persist/es/persistStore';
 
+const rootReducer= combineReducers({user:useReducer})
+const persistConfig={
+    key:"root",
+    storage,
+    version:1
+}
 
-const store = configureStore({
-  reducer: {user:useReducer}, // Add your slices here, e.g., counter: counterReducer
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+export const store = configureStore({
+//   reducer: {user:useReducer}, // Add your slices here, e.g., counter: counterReducer
+    reducer: persistedReducer, 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Disable serializable check if necessary
     }),
 });
 
-export default store;
+export const persistor=persistStore(store);
